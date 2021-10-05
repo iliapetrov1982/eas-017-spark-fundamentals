b95206907428e078d6bd0c9332de8f2e8d5acebd
dextravaganz <denis.m.smirnov@gmail.com>
2021-10-05 13:07:58 +0300

    Add intermediate result

--- src/main/java/com/luxoft/training/eas017/day1/TransformationsAndActions.java
+++ src/main/java/com/luxoft/training/eas017/day1/TransformationsAndActions.java
@@ -2,6 +2,7 @@ package com.luxoft.training.eas017.day1;
 
 import org.apache.commons.io.output.NullPrintStream;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 import java.util.List;
@@ -16,6 +17,29 @@ public class TransformationsAndActions {
         final JavaSparkContext sc = new JavaSparkContext(conf);
 
         List<Integer> numbers = IntStream.range(0, 10).boxed().collect(Collectors.toList());
+
+        JavaRDD<Integer> rdd = sc.parallelize(numbers);
+
+        numbers.add(15);
+
+        System.out.println("rdd contents: " + rdd.collect());
+
+        numbers.add(17);
+
+        System.out.println("rdd contents: " + rdd.collect());
+
+
+
+        //List<Integer> localListOnDriver = rdd.collect();
+
+//        JavaRDD<Integer> evenNumbersRdd = rdd.filter(n -> n % 2 == 0);
+//
+//        evenNumbersRdd.collect();
+//
+//        evenNumbersRdd.map(i -> i+100).collect();
+
+//        JavaRDD<Integer> mappedRdd = rdd.map(i -> i+100);
+//        System.out.println("rdd contents: " + mappedRdd.collect() );
 
    }
 
